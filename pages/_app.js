e363f065fe24a3b99036ebc2710499b7ac5a2d93
unknown <tejas@nickelbronx.com>
2024-01-24 22:38:57 +0530

    video responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import stylemain from '../styles/main.css';
 import stylecustom from '../styles/custom.css';
+import customresponsive from '../styles/responsivecustom.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
